de716674c6d7b0bdbfb8384dc3beb4c39a2f945e
Gucci205 <lwinmoeaung20005@gmail.com>
2026-06-20 01:23:10 +0700

    mobile navigation finished

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,11 +1,13 @@
 import MainNavigation from "./MainNavigation"
 import {siteConfig} from "@/config/site";
+import MobileNavigation from "./MobileNavigation";
 
 function Header() {
   return (
     <header className='w-full border-b'>
-        <nav className='container flex items-center h-16'>
+        <nav className='container flex items-center h-16 mx-auto'>
             <MainNavigation items={siteConfig.mainNav}/>
+            <MobileNavigation items={siteConfig.mainNav}/>
         </nav>
     </header>
   )
--- src/components/layouts/MobileNavigation.tsx
+++ src/components/layouts/MobileNavigation.tsx
@@ -1,7 +1,79 @@
-function MobileNavigation() {
-  return (
-    <div>MobileNavigation</div>
-  )
+import {Link} from "react-router-dom";
+
+import type {MainNavItem} from '@/types';
+import {Icons} from '@/components/icons';
+import { siteConfig } from '@/config/site';
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetClose,
+} from "@/components/ui/sheet";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+interface MainNavigationProps{
+    items?: MainNavItem[]
 }
 
-export default MobileNavigation
+export default function MainNavigation({items}: MainNavigationProps) {
+  return (
+    <div className="lg:hidden">
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="ghost" size="icon" className="ml-4 size-5">
+                    <Icons.menu aria-hidden="true"/>
+                    <span className="sr-only">Toggle Menu</span>
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="pt-9">
+                <SheetClose  asChild>
+                    <Link to="/" className="flex items-center">
+                        <Icons.logo className="size-4 mr-2"/>
+                        <span className="font-bold">{siteConfig.name}</span>
+                        <span className="sr-only"></span>
+                    </Link>
+                </SheetClose>
+                <ScrollArea className="my-4 h-[cal(100vh - 8rem)] pb-8">
+                    <Accordion
+                        type="multiple"
+                        className="w-full"
+                        >
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger>{items?.[0].title}</AccordionTrigger>
+                            <AccordionContent>
+                                <div className="flex flex-col space-y-2 pl-2">
+                                    {items?.[0].card?.map((item) => (
+                                        <SheetClose asChild key={item.title}>
+                                            <Link to={String(item.href)} className="text-foreground/70">
+                                            {item.title}
+                                            </Link>
+                                        </SheetClose>
+                                    ))}
+                                </div>
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                    <div className="flex flex-col space-y-2 mt-4">
+                        {items?.[0].menu?.map((item) => (
+                            <SheetClose asChild key={item.title}>
+                                <Link to={String(item.href)} className="">
+                                {item.title}
+                                </Link>
+                            </SheetClose>
+                        ))}
+                    </div>
+                </ScrollArea>
+            </SheetContent>
+        </Sheet>
+    </div>
+  )
+}
